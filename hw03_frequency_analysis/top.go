707ba6ea7716d2b36ba24fcac4f82ca8961de6d0
Kirill Kotov <trukan24@gmail.com>
2023-02-12 20:37:26 +0300

    :hammer: done hw03

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,52 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type Pair struct {
+	Key   string
+	Value int
+}
+
+var re = regexp.MustCompile(`[.,()!?'"-]+`)
+
+func Top10(s string) []string {
+	// get top 10 words from string
+	data := strings.Fields(s)
+
+	if len(data) == 0 {
+		return []string{}
+	}
+
+	wordFrequency := make(map[string]int)
+	for _, word := range data {
+		// Remove unwanted characters like .,()- from the word
+		word = re.ReplaceAllString(word, "")
+		if len(word) == 0 {
+			continue
+		}
+		wordFrequency[strings.ToLower(word)]++
+	}
+
+	pairs := make([]Pair, 0, len(wordFrequency))
+	for key, value := range wordFrequency {
+		pairs = append(pairs, Pair{key, value})
+	}
+
+	sort.Slice(pairs, func(i, j int) bool {
+		if pairs[i].Value == pairs[j].Value {
+			return pairs[i].Key < pairs[j].Key
+		}
+		return pairs[i].Value > pairs[j].Value
+	})
+
+	var result []string
+	for i := 0; i < 10; i++ {
+		result = append(result, pairs[i].Key)
+	}
+
+	return result
 }
